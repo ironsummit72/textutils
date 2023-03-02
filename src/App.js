0d294aecfc4adb77574bfd7a22f83ac9500f4979
ironsummit72 <ironsumit72@gmail.com>
2023-03-02 14:12:07 +0530

    dark mode feature added

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ import NavBar from "./components/NavBar";
 
 
 function App() {
+  const onChange=(event)=>{
+    if(event.target.checked===true)
+    {
+      document.body.className='dark-theme'
+      document.getElementById('textar').style.backgroundColor='#07224e'
+      document.getElementById('textar').style.color='#ffffff'
+    }else{
+      document.body.className='light-theme'
+      document.getElementById('textar').style.backgroundColor='#ffffff'
+      document.getElementById('textar').style.color='#000000'
+    }
+
+  }
 
   return (
     <>
-      <NavBar />
+      <NavBar toggleOnChange={onChange}/>
 
       <Capatalize />
     </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import "../App.css";
 
-export default function NavBar() {
+export default function NavBar(props) {
   return (
     <>
       <nav class="navbar navbar-expand-lg navbar-light bg-primary ">
@@ -10,7 +10,7 @@ export default function NavBar() {
         <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
        <div className="darkmodetoggle">
-       <input type="checkbox" />
+       <input type="checkbox"  onChange={props.toggleOnChange}/>
         <label htmlFor="darkmode" className="darkmode-label text-white">Enable DarkMode</label>
        </div>
       </li>
