533894df9951beca9842429116d243cbcabe7fe4
ironsummit72 <ironsumit72@gmail.com>
2023-03-02 21:22:28 +0530

    added color palettes and buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,6 +16,16 @@ export default function NavBar(props) {
               </label>
             </div>
           </li>
+          <li>
+            <div className="colorpalettes">
+              <button className="colorbtns darkpurple" style={{backgroundColor:"#2e0065",color:'#984eff'}}>DP</button>
+              <button className="colorbtns darkblue"  style={{backgroundColor:"#040477",color:'#4f39f7'}}>DB</button>
+              <button className="colorbtns darkgreen"style={{backgroundColor:"#004d00",color:'#00e200'}}>DG</button>
+              <button className="colorbtns darkpink"style={{backgroundColor:"#79005e",color:'#f86cdc'}}>DP</button>
+
+
+            </div>
+          </li>
         </ul>
       </nav>
     </>
